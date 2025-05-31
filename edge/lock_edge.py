27c51c135b473a8875d6e83a9be9541c84b1c868
cloud-vinny <cloudvinny03@gmail.com>
2025-05-31 22:46:34 +1000

    Update lock_edge.py cuz this is a bug

--- edge/lock_edge.py
+++ edge/lock_edge.py
@@ -87,7 +87,7 @@ def on_message(client, userdata, msg):
     print(f"[MQTT] Received: {command}")
 
     if "unlock door" in command:
-        sio.emit("unlock_door")
+    unlock_door(True)        # ✅ Directly triggers the function in the same file
 
     elif "turn off light" in command:
         # ser.write(b"LIGHT_OFF\n")
